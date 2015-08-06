1b78ad194895028369f6f83c8d8fdf96c7939fa6
Paul Grever <paulgrever@gmail.com>
2015-08-05 23:11:25 -0600

    learned about templates and isServer and isClient

--- leaderboard.js
+++ leaderboard.js
@@ -1,23 +1,11 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+PlayersList = new Mongo.Collection('players');
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+console.log("paul g!!");
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+if(Meteor.isClient){
+  console.log("Hello client");
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+if(Meteor.isServer){
+  console.log("Hello server");
 }
